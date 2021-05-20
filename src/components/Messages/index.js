883e223b97041e6da60c82eaf7f89a7745c87d29
Brice Guillozet <brice.guillozet@gmail.com>
2021-05-20 13:52:40 +0200

    props ok

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Message from './Message';
 
 import './styles.scss';
 
-import Message from './Message';
-
-const Messages = () => (
+const Messages = ({ messages }) => (
   <div className="messages">
-    <Message />
-    <Message />
-    <Message />
+    {messages.map((message) => (
+      <Message {...message} key={message.id} />
+    ))}
+    <Message author="Me" message="Coucou ça va ?" />
+    <Message author="Other" message="Super et toi ?" isOther />
+    <Message author="Me" message="Ca va !" />
   </div>
 );
+
+Messages.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+Messages.defaultProps = {
+  messages: [],
+};
 
 export default Messages;
